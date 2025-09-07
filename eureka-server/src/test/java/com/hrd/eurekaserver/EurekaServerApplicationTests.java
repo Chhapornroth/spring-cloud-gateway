8e78189d030cc9928bb7f92a49c9f0052a8cbea0
chhapornroth <choponrothyin11111@gmail.com>
2025-09-08 06:00:00 +0700

    serrver

---
+++ eureka-server/src/test/java/com/hrd/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrd.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
